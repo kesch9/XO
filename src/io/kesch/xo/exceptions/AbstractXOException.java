4f2524da7a6d0eac0e91d8c61341fc8632ededc3
kesch9 <kashenkovsergej@yandex.ru>
2016-02-09 18:19:48 +0400

    Add - create Exception

---
+++ src/io/kesch/xo/exceptions/AbstractXOException.java
@@ -0,0 +1,7 @@
+package io.kesch.xo.exceptions;
+
+/**
+ * Created by user on 09.02.16.
+ */
+public abstract class AbstractXOException extends Exception {
+}
---
+++ src/io/kesch/xo/exceptions/InvalidePointExceptionAbstract.java
@@ -0,0 +1,7 @@
+package io.kesch.xo.exceptions;
+
+/**
+ * Created by user on 09.02.16.
+ */
+public class InvalidePointExceptionAbstract extends AbstractXOException {
+}
