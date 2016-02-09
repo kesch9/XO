bd413e89f9bcbbc03c44bc34cc0816925719794b
kesch9 <kashenkovsergej@yandex.ru>
2016-02-09 18:20:28 +0400

    Add - checkCoordinate in Field

--- src/io/kesch/xo/Field.java
+++ src/io/kesch/xo/Field.java
@@ -8,6 +8,8 @@ import java.awt.*;
 public class Field {
 
     private static final int FIELD_SIZE = 3;
+    private static final int MIN_COORDINATE = 0;
+    private static final int MAX_COORDINATE = FIELD_SIZE;
     private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
 
     public int getSize(){
@@ -20,6 +22,14 @@ public class Field {
 
     public void setFigure(final Point point, final Figure figure){
         field[point.x][point.y]=figure;
+    }
+
+    private boolean checkPoint(final Point point){
+        return checkCoordinate(point.x)&&checkCoordinate(point.y);
+    }
+
+    private boolean checkCoordinate(final int coordinate){
+        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
     }
 
 }
