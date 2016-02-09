8e8164a0b53851c5218aa5d1b8078429d5d1ebaf
kesch9 <kashenkovsergej@yandex.ru>
2016-02-09 17:13:04 +0400

    Add - Player Test

---
+++ tst/io/kesch/xo/PlayerTest.java
@@ -0,0 +1,18 @@
+package io.kesch.xo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFigure() throws Exception {
+
+    }
+}
